0a0b682775662f791258fa02483a715b4e87a6cd
Joli Holmes <holmesjoli@gmail.com>
2022-01-06 20:05:23 -0500

    updates to js

---
+++ js/page_elements.js
@@ -0,0 +1,8 @@
+$(function(){
+    $("#nav").load("header.html"); 
+    $("footer").load("footer.html"); 
+    });
+
+function resizeIframe(obj) {
+    obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
+}
