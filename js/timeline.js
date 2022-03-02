0e600b1598998e33e81d3009c6c8cceea2da02b5
Joli Holmes <holmesjoli@gmail.com>
2022-03-02 14:04:36 -0500

    updated with data to fill strokes more easily

--- js/timeline.js
+++ js/timeline.js
@@ -1,12 +1,25 @@
-function showTimeline(id, nWeeks) {
+function showTimeline(id, nWeeks, fillWeek = 0) {
 
-    let width = 50*nWeeks;
+    let width = 50*(nWeeks -1) + 30;
     let height = 70;
 
-    const nArray = [];
+    const data = [];
+    let fill;
+    let stroke;
     for (var i = 1; i <= nWeeks; i++) {
-        nArray.push(i);
+
+        if (fillWeek === i) {
+            fill = "#FF4081";
+            stroke = "#000000";
+        } else {
+            fill = "#FFFFFF";
+            stroke = "#000000";
+        }
+
+        data.push({week: i, fill: fill, stroke: stroke});
     }
+
+    console.log(data);
 
     let svg = d3.select(id)
         .append("svg")
@@ -16,28 +29,31 @@ function showTimeline(id, nWeeks) {
     svg.append("line")
         .attr("x1", 25)
         .attr("y1", height - 55)
-        .attr("x2", 50*(nArray.length -1) + 15)
+        .attr("x2", 50*(data.length -1) + 5)
         .attr("y2", height - 55)
         .attr("stroke", "black")
 
-    nArray.forEach(function(d, i) {
+    svg.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", function(d, i) {return 50*i + 15;})
+        .attr("cy", height - 55)
+        .attr("r", 12)
+        .attr("fill", function(d) {return d.fill;})
+        .attr("stroke", function(d) {return d.stroke;})
 
-        svg.append("circle")
-            .attr("cx", 50*i + 15)
-            .attr("cy", height - 55)
-            .attr("r", 12)
-            .attr("fill", "white")
-            .attr("stroke", "black")
-
-        svg.append("text")
-            .attr("x", 50*i + 15)
-            .attr("y", height - 25)
-            .attr("text-anchor", "middle")
-            .text(i)
-    });
+    svg.selectAll("text")
+        .data(data)
+        .enter()
+        .append("text")
+        .attr("x", function(d, i) {return 50*i + 15;})
+        .attr("y", height - 25)
+        .attr("text-anchor", "middle")
+        .text(function(d, i) {return i + 1;})
 
     svg.append("text")
-        .attr("x", width/2 - 10)
+        .attr("x", width/2)
         .attr("y", height - 5)
         .attr("text-anchor", "middle")
         .text("Weeks")
