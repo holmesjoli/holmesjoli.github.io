009baee59171d6b3ceb8f2f530c23afb537244b2
Joli Holmes <holmesjoli@gmail.com>
2023-07-09 17:39:56 -0400

    added bar chart

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,94 @@
+import { useEffect } from "react";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
+import * as d3 from 'd3';
+
+const data = [
+    {tool: "R", year: 0, years: 7},
+    {tool: "Python", year: 0, years: 3},
+    {tool: "HTML", year: 0, years: 3},
+    {tool: "CSS", year: 0, years: 2},
+    {tool: "JS", year: 0, years: 2},
+    {tool: "Git", year: 0, years: 4},
+    {tool: "Illustrator", year: 0, years: 2},
+    {tool: "XD", year: 0, years: 1}
+];
+
+var margin = {top: 20, right: 30, bottom: 50, left: 90},
+width = 460 - margin.left - margin.right,
+height = 400 - margin.top - margin.bottom;
+
+function barChart() {
+    
+    var svg = d3.select("#visualization")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    var x = d3.scaleLinear()
+        .domain([0, 7])
+        .range([ 0, width]);
+
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x).ticks(7))
+        .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+
+    // Y axis
+    var y = d3.scaleBand()
+        .range([ 0, height ])
+        .domain(data.map(function(d) { return d.tool; }))
+        .padding(.1);
+
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    //Bars
+    let bars = svg.selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", x(0))
+        .attr("y", function(d) { return y(d.tool); })
+        .attr("width", function(d) { return x(d.year); })
+        .attr("height", y.bandwidth() )
+        .attr("fill", "#69b3a2");
+
+    bars.transition()
+        .duration(3000)
+        .attr("width", function(d) { return x(d.years);});
+
+    svg.append("text")
+        .attr("class","axisLabel")
+        .attr("x", margin.left + (width-margin.left-margin.right)/2)
+        .attr("y", height + 38)
+        .attr("text-anchor","middle")
+        .text("Years of Experience");
+
+    svg.append("text")
+        .attr("class","axisLabel")
+        .attr("x", -(height-margin.bottom)/2)
+        .attr("y", margin.left - 150)
+        .attr("text-anchor","middle")
+        .attr("transform","rotate(-90)")
+        .text("Tools");
+}
 
 export default function About() {
+
+    useEffect(() => {
+        barChart()
+    }, [])
 
     return(
         <div className="Content">
             <Navigation />
+            <div id="visualization"></div>
             <Footer />
         </div>
     )
