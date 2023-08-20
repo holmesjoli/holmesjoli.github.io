6666fe337a36cebb6262d87e0c33e4101a2f697c
Joli Holmes <holmesjoli@gmail.com>
2023-08-19 21:51:10 -0400

    added new assets

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,5 +1,6 @@
 export let itemData = [
     {title: "Algorithmically Fair", year: 2022, src: "/assets/algorithmically-fair/image1.jpg", alt: "Three dimensional data visualization comparing black recidivism rates to white recidivism rates"},
     {title: "Black Data", year: 2020, src: "/assets/algorithmically-fair/image1.jpg", alt: ""},
+    {title: "Banca Intesa Processes", year: "2023", src: "/assets/banca-intesa-processes/circle-packing-full-screen.png", alt: "A data visualization showing multiple levels of organizational hierarchy."}
 ];
 
