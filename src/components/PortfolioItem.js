a6e489fd49821382f31d8983f85092db02281a8b
Joli Holmes <holmesjoli@gmail.com>
2023-08-20 01:38:47 -0400

    initialized new pages

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -5,6 +5,7 @@ export default function PortfolioItem({title, year, src, alt}) {
             <h2 className="Item-Title">{title}</h2>
             <h3 className="Item-Year">{year}</h3>
             <img className="Item-Image" src={src} alt={alt} />
+            <div class="Tag-Container"></div>
         </div>
     )
 }
