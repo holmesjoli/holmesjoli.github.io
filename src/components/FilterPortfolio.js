b5c76654a28ab25b81b5ee5701117a2a448a875b
Joli Holmes <holmesjoli@gmail.com>
2023-08-28 19:15:30 -0400

    added potential filters

--- src/components/FilterPortfolio.js
+++ src/components/FilterPortfolio.js
@@ -1,35 +1,34 @@
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+
 export default function FilterPortfolio({data, updateData}) {
 
 
     const filterSelection = (e) => {
 
-        // console.log(e);
+        console.log(e);
         
         // const dataNew = data.filter(d => d.tags.includes(e));
 
         // updateData(dataNew);
     }
 
-    return(
+    const filters = ["#adobe-illustrator", "#ceramics", "#collection", "#d3-js", "#data-art", "#data-journalism",
+    "#data-physicalization", "#data-visualization", "#design-process", "#drawing", "#figma", "#information-design",
+    "#interaction-design", "#graphic-design", "#map", "#pixi-js", "#public-policy", "#python", "#r", "#react",
+    "#system-design", "#service-design", "#user-experience", "#user-interaction" ]
+
+    return (
         <div className="Button-Container">
-            <h3 className="filter-title">Filter items</h3>
-            <button className="btn active" onClick={filterSelection('all')}>All</button>
-            <button className="btn" onClick={filterSelection('adobe-illustrator')}>#adobe-illustrator</button>
-            <button className="btn" onClick={filterSelection('ceramics')}>#ceramics</button>
-            <button className="btn" onClick={filterSelection('d3-js')}>#d3-js</button>
-            <button className="btn" onClick={filterSelection('data-art')}>#data-art</button>
-            <button className="btn" onClick={filterSelection('data-visualization')}>#data-visualization</button>
-            <button className="btn" onClick={filterSelection('data-physicalization')}>#data-physicalization</button>
-            <button className="btn" onClick={filterSelection('drawing')}>#drawing</button>
-            <button className="btn" onClick={filterSelection('figma')}>#figma</button>
-            <button className="btn" onClick={filterSelection('information-design')}>#information-design</button>
-            <button className="btn" onClick={filterSelection('interaction-design')}>#interaction-design</button>
-            <button className="btn" onClick={filterSelection('graphic-design')}>#graphic-design</button>
-            <button className="btn" onClick={filterSelection('map')}>#map</button>
-            <button className="btn" onClick={filterSelection('public-policy')}>#public-policy</button>
-            <button className="btn" onClick={filterSelection('python')}>#python</button>
-            <button className="btn" onClick={filterSelection('r')}>#r</button>
-            <button className="btn" onClick={filterSelection('system-design')}>#system-design</button>
+             <h3 className="filter-title">Filter items</h3>
+            <FormGroup >
+                {
+                    filters.map(d => {
+                        return <FormControlLabel control={<Checkbox defaultChecked size="small" />} label={d} />
+                    })
+                }
+            </FormGroup>
         </div>
-    )
+    );
 }
