4c8aaac40947fe99db9129e5404d1b40eb7876b9
Joli Holmes <holmesjoli@gmail.com>
2023-08-29 17:54:33 -0400

    added under construction symbol to the individual pages

--- src/pages/portfolio/Algorithmically-Fair.js
+++ src/pages/portfolio/Algorithmically-Fair.js
@@ -6,6 +6,9 @@ export default function AlgorithmicallyFair() {
     return(
         <div className="Main">
             <Navigation />
+            <div className="Construction">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Under_construction_animated.gif" alt="Page under construction image"></img>
+            </div>
             <Footer/>
         </div>
     )
