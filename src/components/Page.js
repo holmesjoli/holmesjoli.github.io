6b98350936d6fa946c1c00538422c466e74d10e3
Joli Holmes <holmesjoli@gmail.com>
2023-09-09 12:49:27 -0400

    restructured data

--- src/components/Page.js
+++ src/components/Page.js
@@ -65,27 +65,35 @@ export default function Page({d}) {
                     <div className="Project-Design">
                         <h3>design process</h3>
                         {
-                            d.sketches.length > 0  ? 
-                            <div className="Project-Sketches">
+                            d.designProcess.research.length > 0  ? 
+                            <div className="Project-Design-Research SlideShow">
+                                <h4>design research</h4>
+                                <Slideshow data={d.designProcess.research}/>
+                            </div>: <></>
+                        }
+                        {
+                            d.designProcess.sketches.length > 0  ? 
+                            <div className="Project-Sketches SlideShow">
                                 <h4>initial sketches</h4>
-                                <Slideshow data={d.sketches}/>
+                                <Slideshow data={d.designProcess.sketches}/>
                             </div>: <></>
                         }
                         {
-                            d.prototype.length > 0  ? 
-                            <div className="Project-Prototype">
+                            d.designProcess.prototype.length > 0  ? 
+                            <div className="Project-Prototype SlideShow">
                                 <h4>prototypes</h4>
-                                <Slideshow data={d.prototype}/>
+                                <Slideshow data={d.designProcess.prototype}/>
                             </div>: <></>
                         }
                         {
-                            d.final.length > 0  ? 
-                            <div className="Project-Final">
+                            d.designProcess.final.length > 0  ? 
+                            <div className="Project-Final SlideShow">
                                 <h4>final design</h4>
-                                <MainImage data={d.final.filter(e => e.size === "big")}/>
-                                <Slideshow data={d.final.filter(e => e.size === "small")}/>
+                                <MainImage data={d.designProcess.final.filter(e => e.size === "big")}/>
+                                <Slideshow data={d.designProcess.final.filter(e => e.size === "small")}/>
                             </div>:
-                            <div className="Construction">
+                            <div className="Construction SlideShow">
+                                <h4>final design</h4>
                                 <img src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Under_construction_animated.gif" alt="Page under construction image"></img>
                             </div>
                         }
