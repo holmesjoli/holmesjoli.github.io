61b455f4ad4c3b40b33a867337209e06fba6fac7
Joli Holmes <holmesjoli@gmail.com>
2023-09-20 14:20:44 -0700

    added initial media queries to get pages into column format

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -7,7 +7,7 @@ import Footer from "../components/Footer";
 import { PortfolioItem } from "../components/PortfolioItem";
 import FilterPortfolio from "../components/FilterPortfolio";
 
-import { itemData, toolFilters, designFilters, awardFilters } from "../utils/global";
+import { itemData, toolFilters, designFilters } from "../utils/global";
 
 function filterData(selected, d) {
     let selectedInList = [];
@@ -31,7 +31,6 @@ export default function Portfolio() {
 
     const [selectedTools, updateSelectedTools] = useState([]);
     const [selectedDesign, updateSelectedDesign] = useState([]);
-    const [selectedAward, updateSelectedAward] = useState([]);
 
     useEffect(() => {
 
@@ -40,7 +39,7 @@ export default function Portfolio() {
             if (selectedTools.length === 0 && selectedDesign.length === 0) {
                 return d;
             } else {
-                return filterData(selectedAward.concat(selectedTools.concat(selectedDesign)), d);
+                return filterData(selectedTools.concat(selectedDesign), d);
             }
         });
 
@@ -55,7 +54,6 @@ export default function Portfolio() {
                 <div>
                     <FilterPortfolio title={"tools"} filters={toolFilters} updateSelectedValues={updateSelectedTools} selectedValues={selectedTools}/>
                     <FilterPortfolio title={"design"} filters={designFilters} updateSelectedValues={updateSelectedDesign} selectedValues={selectedDesign}/>
-                    <FilterPortfolio title={"award"} filters={awardFilters} updateSelectedValues={updateSelectedAward} selectedValues={selectedAward}/>
                 </div>
                 <div className="Item-Container">
                     {
