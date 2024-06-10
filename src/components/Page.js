9ff3b42d7ed323cbe0598833534a26e1c6b5544e
Joli Holmes <holmesjoli@gmail.com>
2024-06-10 08:43:38 -0700

    initial commit of new nav;

--- src/components/Page.js
+++ src/components/Page.js
@@ -3,45 +3,101 @@ import { DesignStage, CollectionExample } from "./Carousel"
 import Navigation from "./Navigation";
 import Footer from "./Footer";
 import SideBarRight from "./SideBarRight";
+import * as d3 from 'd3';
+
+// Update current placement of all navigation items based on current active item
+// TODO: persistent rotational direction
+function updateRotation(activeIndex, lastActiveIndex) {
+
+    // define rotation increments based on number of items in nav
+    // TODO: programmatically decide increment value
+    const rotaIncrement = 60;
+    // Control where on the wheel active nav item is displayed
+    const activeNavItemPos = 135;
+
+    let rotaVal = 0, rotaValInverse = 0, currIndex;
+
+    // Find the offset value for all items based on active nav item
+    var rotaOffset = activeIndex * rotaIncrement;
+
+    // Bring appropriate content into view
+    d3.select('#main-content .active').removeClass('active');
+    d3.select('#main-content .item:nth-child('+activeIndex+')').addClass('active');
+
+    // Iterate through all nav items and update their position/rotation
+    d3.select('#nav .item').each(function() {
+        currIndex = this.index()+1;
+        // calculate current rotational value of item
+        rotaVal = rotaIncrement*currIndex-rotaOffset+activeNavItemPos;
+        // Inverse rotaVal to counter-rotate text/image in relation to nav item parent
+        rotaValInverse = (rotaIncrement*currIndex-rotaOffset+activeNavItemPos)*(-1);
+
+        this.data('rotaVal', rotaVal);
+
+        this.css({
+            'transform': 'rotate('+rotaVal+'deg)'
+        });
+        this.find('.sub').css({
+            'transform': 'rotate('+rotaValInverse+'deg)'
+        });
+    });
+}
 
 function PageNavigation() {
 
+    // document.ready(function() {
+    //     // Find currently active item and pass it to item placement function
+    //     var initActiveIndex = d3.select('.item.active').index()+1;
+    //     d3.select('#main-content .item:nth-child('+initActiveIndex+')')
+    //     updateRotation(initActiveIndex, 0);
+
+    //     // Update active nav item and rotate it to the top
+    //     d3.select('.item').click(function() {
+    //         var activeIndex = this.index()+1;
+    //         var lastActiveIndex = d3.select('.active').index()+1;
+    //         d3.select('.active').removeClass('active');
+    //         this.addClass('active');
+
+    //         updateRotation(activeIndex, lastActiveIndex);
+    //     })
+    // });
+
     return (
-        <nav class="radial-nav">
-        <div class="center"></div>
-        <ul>
-            <li>
-            <a href="#">
-                <i class="fa fa-chrome"></i>
-            </a>
-            </li>
-            <li>
-            <a href="#">
-                <i class="fa fa-firefox"></i>
-            </a>
-            </li>
-            <li>
-            <a href="#">
-                <i class="fa fa-opera"></i>
-            </a>
-            </li>
-            <li>
-            <a href="#">
-                <i class="fa fa-edge"></i>
-            </a>
-            </li>
-            <li>
-            <a href="#">
-                <i class="fa fa-safari"></i>
-            </a>
-            </li>
-            <li>
-            <a href="#">
-                <i class="fa fa-internet-explorer"></i>
-            </a>
-            </li>
-        </ul>
-    </nav>
+        <div id="nav">
+            <div id="nav-bg"><div class="inner"></div></div>
+                <div id="nav-inside">
+                    <div className="item item-1 active">
+                        <div className="sub">
+                            <p>1</p>
+                        </div>
+                    </div>
+                    <div className="item item-2">
+                        <div className="sub">				
+                            <p>2</p>
+                        </div>
+                    </div>
+                    <div className="item item-3">
+                        <div className="sub">
+                            <p>3</p>
+                        </div>
+                    </div>
+                    <div className="item item-4">			
+                        <div className="sub">				
+                            <p>4</p>
+                        </div>
+                    </div>
+                    <div className="item item-5">
+                        <div className="sub">
+                            <p>5</p>
+                        </div>
+                    </div>
+                    <div className="item item-6">			
+                        <div className="sub">				
+                            <p>6</p>
+                        </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
